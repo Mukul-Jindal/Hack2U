6a2d6814ba1b04f60ed89e6730678a1d74853c93
Mukul-Jindal <Mukul-Jindal@github.com>
2024-02-09 19:34:15 +0530

    started routing

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import {
   Route,
   Routes,
 } from 'react-router-dom'
+import { Home } from './pages/Home/home';
 
 function App() {
   const [isSignIn, setSignIn] = useState(false);
@@ -23,17 +24,9 @@ function App() {
       <UserState>
         <Router>
           <Routes>
-
-            {/* <SignIn setSignIn={setSignIn} /> */}
+            <Route exact path='/' element = {<Home/>}/>
             <Route exact path='/login' element={<Login/>} />
             <Route exact path='/register' element={<Sign/>} />
-            {/* <SignUp/> */}
-            {/* <UploadImage />
-          {
-            isSignIn
-            ? <ClothesPage />
-            : <></>
-          } */}
           </Routes>
         </Router>
       </UserState>
